8a252773889731597addc4c8b1d5dbbfc6c49ec1
PNGT-DEV <50997058+PNGT-DEV@users.noreply.github.com>
2020-11-11 17:40:12 +0200

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'adamant'
+games { 'gta5' }
+
+description 'Ilotulitteet'
+version '1.0'
+
+client_scripts {
+	'client/main.lua'
+}
+
+server_scripts {
+	'server/main.lua'
+}
---
+++ ilotulitteet.sql
@@ -0,0 +1,6 @@
+INSERT INTO `items` (`name`, `label`, `limit`, `rare`, `can_remove`) VALUES
+	('trailburst', 'Legend (Ilotulite)', 5, 0, 1),
+	('fountain', 'Tulivuori (Ilotulite)', 5, 0, 1),
+	('starburst', 'Normaali Raketti', 5, 0, 1),
+	('shotburst', 'V.I.P (Ilotulite)', 5, 0, 1)
+;
